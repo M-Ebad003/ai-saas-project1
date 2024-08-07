b2c433590e19b9c87d525cbf20f0d1c216c50db6
ebad <ebad003@gmail.com>
2024-08-07 15:40:10 +0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 //     auth().protect();
 //   }
 // });
-
+export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
